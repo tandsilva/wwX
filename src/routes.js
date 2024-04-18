e0250590e93bd5931e9e443b63db715fd0701fbf
Thiago Aldrin Marques da Silva <65571896+tandsilva@users.noreply.github.com>
2024-04-18 11:35:54 -0300

    Routes

--- src/routes.js
+++ src/routes.js
@@ -1,11 +1,16 @@
 import { Router } from "express";
 
-const { request, response } = require("./app");
-
+//const { request, response } = require("./app");
+import User from './app/models/User';
 const routes = new Router();
 
-routes.get("/", (request, response) =>
-  response.status(200).json({ message: "Ola Mundo" }),
-);
+routes.get("/",async (request, response) =>{
+  const user = await User.create({
+    name:'MAristela',
+    email: 'aldrinmarques.s.i@gmail.com',
+    password_hash:'Educacional9939'
+  });
+  return response.status(201).json(user);
+});
 
 export default routes;
