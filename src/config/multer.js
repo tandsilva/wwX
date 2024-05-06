e894cfed0c0158d3219d76078406ecb2f5db4d76
Thiago Aldrin Marques da Silva <65571896+tandsilva@users.noreply.github.com>
2024-05-06 09:55:34 -0300

    Upload de imagens

---
+++ src/config/multer.js
@@ -0,0 +1,14 @@
+import multer  from "multer";
+import { v4 } from "uuid";
+import {extname,resolve} from "node:path"
+
+export default{
+    storage: multer.diskStorage({
+        destination: resolve(__dirname,"..","..","uploads"),
+        filename:(request,file,callback)=>
+             callback(null,v4()+ extname(file.originalname)),
+
+        
+
+    }),
+};
--- src/routes.js
+++ src/routes.js
@@ -2,10 +2,15 @@ import { Router } from 'express';
 import UserController from './app/controllers/UserController';
 import SessionController from './app/controllers/SessionController';
 import ProductController from './app/controllers/ProductController';
+import multer from 'multer';
+import multerConfig from "./config/multer"
+
 
 const routes = Router();
+const upload = multer(multerConfig);
+
 
 routes.post('/users', UserController.store);
 routes.post('/session', SessionController.store);
-routes.post('/products',ProductController.store);
+routes.post('/products', upload.single("file"), ProductController.store);
 export default routes;
