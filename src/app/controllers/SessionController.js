d5c509a01155d57ba07ed87a23e76f2483e3a1d8
Thiago Aldrin Marques da Silva <65571896+tandsilva@users.noreply.github.com>
2024-05-13 10:43:00 -0300

    JSONWEBTOKEN

--- src/app/controllers/SessionController.js
+++ src/app/controllers/SessionController.js
@@ -1,5 +1,7 @@
 import * as Yup from 'yup';
 import User from '../models/User'
+import jwt from 'jsonwebtoken'
+import authConfig from '../../config/auth'
 class SessionController {
     async store(request, response) {
         const schema = Yup.object({
@@ -33,8 +35,15 @@ class SessionController {
 
 
 
-        return response.status(201).json({ id: user.id ,name: user.name,email,
-        admin: user.admin,
+        return response.status(201).json({ id: user.id ,
+            name: user.name,
+            email,
+            admin: user.admin,
+            token: jwt.sign({id: user.id},authConfig.secret,
+            {
+                expiresIn: authConfig.expiresIn,  
+            })
+
     })
     }
 };
