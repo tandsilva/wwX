732dde32256de7be9e48913045436b4e7e5d4416
Thiago Aldrin Marques da Silva <65571896+tandsilva@users.noreply.github.com>
2024-05-29 10:41:20 -0300

    Validando duplicacoes categorias

--- src/app/controllers/CategoryController.js
+++ src/app/controllers/CategoryController.js
@@ -17,12 +17,20 @@ class CategoryController{
         }
 
         const {name  } = request.body;
-        const category = await Category.create({
+        const categoryExiste = await Category.findOne({
+           where:{
+            name,
+           },
+        });
+        if (categoryExiste){
+          return response.status(400).json({error:'Categoria ja existe '})
+        }
+        const {id} = await Category.create({
           name,
        
         })
        // console.log(file)
-          return response.status(201).json({category})
+          return response.status(201).json({id,name})
         }
 
         async index(request,response){
