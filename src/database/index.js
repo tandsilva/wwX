e01b4a74fc0e5ca1bf5d34e683fe8ec1fdf8b8fa
Thiago Aldrin Marques da Silva <65571896+tandsilva@users.noreply.github.com>
2024-08-13 11:15:14 -0300

    Mapeamento nao relacional

--- src/database/index.js
+++ src/database/index.js
@@ -3,18 +3,31 @@ import configDataBase from '../config/database'
 import User from "../app/models/User";
 import Product from '../app/models/Product';
 import Category from '../app/models/Category';
+import mongoose from 'mongoose';
 const models =[User,Product,Category];//para mapear 
 //toda vez que ultiizar uma model nao precisara ser feita nova conexao
 //toda vez que criar uma model precisa ser ajustado o index
 class Database{
     constructor(){
         this.init();
+        this.mongo();
     }
     init(){
         this.connection = new Sequelize(configDataBase);
         models.map((model) => model.init(this.connection)).map(model => model.associate && model.associate(this.connection.models) ,
     );
         }
+
+    mongo(){
+        this.mongoConnection =mongoose.connect('mongodb://localhost:27017/produtos');
+
+
+    }
+
+
+
+
+
 }
 
 export default new Database();
